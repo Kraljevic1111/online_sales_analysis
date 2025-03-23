60c811f709555194f82de842faccf171a0b3bfcf
Kraljevic <majgiker@gmail.com>
2025-03-23 09:58:38 +0100

    Changed quantity of products and removing code lines

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ from product import Product
 def main():
     manager = ProductManager()
 
-    manager.add_product(Product("Laptop", 1200.99, 5))
-    manager.add_product(Product("Mouse", 25.50, 15))
-    manager.add_product(Product("Keyboard", 45.00, 10))
-    manager.add_product(Product("Monitor", 250.75, 7))
+    manager.add_product(Product("Laptop", 1200.99, 9))
+    manager.add_product(Product("Mouse", 25.50, 21))
+    manager.add_product(Product("Keyboard", 45.00, 11))
+    manager.add_product(Product("Monitor", 250.75, 12))
 
-    print("Available Products:")
-    manager.display_all_products()
+    #print("Available Products:")
+    #manager.display_all_products()
 
-    total_value = manager.total_inventory_value()
-    print(f"\nTotal Inventory Value: ${total_value:.2f}")
+    #total_value = manager.total_inventory_value()
+    #print(f"\nTotal Inventory Value: ${total_value:.2f}")
     
 
 if __name__ == "__main__":
